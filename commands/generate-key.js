90db9fff81b0a150d561b836695b0b8976adc490
crypto-guys <50590698+crypto-guys@users.noreply.github.com>
2021-02-18 11:26:32 -0600

    Update generate-key.js

--- commands/generate-key.js
+++ commands/generate-key.js
@@ -1,68 +1,21 @@
 const KeyPair = require('near-api-js').KeyPair;
 const exitOnError = require('../utils/exit-on-error');
-const implicitAccountId = require('../utils/implicit-accountid');
 
 module.exports = {
-    command: 'generate-key [account-id]',
-    desc: 'generate key or show key from Ledger',
+    command: 'generate-key <account-id>',
+    desc: 'generate key ',
     builder: (yargs) => yargs,
     handler: exitOnError(async (argv) => {
         let near = await require('../utils/connect')(argv);
-
-        if (argv.usingLedger) {
-            if (argv.accountId) {
-                console.log('WARN: Account id is provided but ignored in case of using Ledger.');
-            }
-            const publicKey = await argv.signer.getPublicKey();
-            // NOTE: Command above already prints public key.
-            console.log(`Implicit account: ${implicitAccountId(publicKey.toString())}`);
-            // TODO: query all accounts with this public key here.
-            // TODO: check if implicit account exist, and if the key doen't match already.
+        if (!argv.accountId) {
             return;
         }
-        const publicKey = await options.signer.getPublicKey();
-        // NOTE: Command above already prints public key.
-        console.log(`Implicit account: ${implicitAccountId(publicKey.toString())}`);
-        // TODO: query all accounts with this public key here.
-        // TODO: check if implicit account exist, and if the key doen't match already.
-        return;
-    }
-                         
-        // If key doesn't exist, create one and store in the keyStore.
-        // Otherwise, it's expected that both key and accountId are already provided in arguments.
-        if (!argv.publicKey) {
-            const keyPair = KeyPair.fromRandom('ed25519');
-            argv.publicKey = keyPair.publicKey.toString();
-            argv.accountId = argv.accountId || implicitAccountId(argv.publicKey);
-            await keyStore.setKey(argv.networkId, argv.accountId, keyPair);
-        } else if (argv.seedPhrase) {
-            const seededKeyPair = await argv.signer.keyStore.getKey(argv.networkId, argv.accountId);
-            await keyStore.setKey(argv.networkId, argv.accountId, seededKeyPair);
+
+        if (argv.usingLedger) {
+            await argv.signer.getPublicKey();
+            // NOTE: Command above already prints public key
+            return;
         }
-            
-        console.log(`Key pair with ${argv.publicKey} public key for an account "${argv.accountId}"`);
-    })
-};
-
-    const { deps: { keyStore } } = near.config;
-    const existingKey = await keyStore.getKey(options.networkId, options.accountId);
-    if (existingKey) {
-        console.log(`Account has existing key pair with ${existingKey.publicKey} public key`);
-        return;
-    }
-
-    // If key doesn't exist, create one and store in the keyStore.
-    // Otherwise, it's expected that both key and accountId are already provided in arguments.
-    if (!options.publicKey) {
-        const keyPair = KeyPair.fromRandom('ed25519');
-        options.publicKey = keyPair.publicKey.toString();
-        options.accountId = options.accountId || implicitAccountId(options.publicKey);
-        await keyStore.setKey(options.networkId, options.accountId, keyPair);
-    }
-            
-    console.log(`Key pair with ${options.publicKey} public key for an account "${options.accountId}"`);
-    
-}
 
         const { deps: { keyStore } } = near.config;
         const existingKey = await keyStore.getKey(argv.networkId, argv.accountId);
@@ -71,19 +24,8 @@ module.exports = {
             return;
         }
 
-        // If key doesn't exist, create one and store in the keyStore.
-        // Otherwise, it's expected that both key and accountId are already provided in arguments.
-        if (!argv.publicKey) {
-            const keyPair = KeyPair.fromRandom('ed25519');
-            argv.publicKey = keyPair.publicKey.toString();
-            argv.accountId = argv.accountId || implicitAccountId(argv.publicKey);
-            await keyStore.setKey(argv.networkId, argv.accountId, keyPair);
-        } else if (argv.seedPhrase) {
-            const seededKeyPair = await argv.signer.keyStore.getKey(argv.networkId, argv.accountId);
-            await keyStore.setKey(argv.networkId, argv.accountId, seededKeyPair);
-        }
-            
-        console.log(`Key pair with ${argv.publicKey} public key for an account "${argv.accountId}"`);
+        const keyPair = KeyPair.fromRandom('ed25519');
+        await keyStore.setKey(argv.networkId, argv.accountId, keyPair);
+        console.log(`Generated key pair with ${keyPair.publicKey} public key`);
     })
 };
-
